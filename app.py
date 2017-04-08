f975b9f8b678bf76c568026680f471deae7535b5
Yu <shaoyenyu@gmail.com>
2017-04-08 17:45:40 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__, static_url_path="")
+
+
+# @app.route('/')
+# def hello_world():
+#     return "Welcome~ This is a website designed by Yu~"
+@app.route('/')
+def hello_world():
+    return app.send_static_file("Apple.html")
+
+
+if __name__ == "__main__":
+    app.run()
